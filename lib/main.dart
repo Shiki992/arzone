428e02a12ac2ed06ebfbf13255fedb8bb63794d6
Hrishikesh Vedantam <shikiikihs2@gmail.com>
2020-12-07 17:24:22 +0530

    submission grade commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,105 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
-import 'package:vector_math/vector_math_64.dart' as vector;
-void main() {
-  runApp(MyApp());
-}
+import 'package:flutter/services.dart';
+
+import 'package:arzone/starting_page.dart';
+
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: HelloWorld(),
-    );
-  }
-}
-
-class HelloWorld extends StatefulWidget {
-  @override
-  _HelloWorldState createState() => _HelloWorldState();
-}
-
-class _HelloWorldState extends State<HelloWorld> {
-  ArCoreController arCoreController;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-//        appBar: AppBar(
-//          title: const Text('Hello World'),
-//        ),
-        body: ArCoreView(
-          onArCoreViewCreated: _onArCoreViewCreated,
+        primaryColor: Colors.deepPurple,
+        accentColor: Colors.red,
+        textTheme: TextTheme(
+            bodyText1: TextStyle(color: Colors.deepPurple, fontSize: 50),
+            headline3: TextStyle(color: Colors.white, fontSize: 25)),
+        dialogTheme: DialogTheme(
+          backgroundColor: Colors.deepPurple,
+          contentTextStyle: TextStyle(color: Colors.white, fontSize: 16),
         ),
+        scaffoldBackgroundColor: Colors.white,
       ),
+      home: StartingPage(),
+      debugShowCheckedModeBanner: false,
     );
-  }
-
-  void _onArCoreViewCreated(ArCoreController controller) {
-    arCoreController = controller;
-
-    _addSphere(arCoreController);
-    _addCylindre(arCoreController);
-    _addCube(arCoreController);
-  }
-
-  void _addSphere(ArCoreController controller) {
-    final material = ArCoreMaterial(
-        color: Color.fromARGB(120, 66, 134, 244));
-    final sphere = ArCoreSphere(
-      materials: [material],
-      radius: 0.1,
-    );
-    final node = ArCoreNode(
-      shape: sphere,
-      position: vector.Vector3(0, 0, -1.5),
-    );
-    controller.addArCoreNode(node);
-  }
-
-  void _addCylindre(ArCoreController controller) {
-    final material = ArCoreMaterial(
-      color: Colors.red,
-      reflectance: 1.0,
-    );
-    final cylindre = ArCoreCylinder(
-      materials: [material],
-      radius: 0.5,
-      height: 0.3,
-    );
-    final node = ArCoreNode(
-      shape: cylindre,
-      position: vector.Vector3(0.0, -0.5, -2.0),
-    );
-    controller.addArCoreNode(node);
-  }
-
-  void _addCube(ArCoreController controller) {
-    final material = ArCoreMaterial(
-      color: Color.fromARGB(120, 66, 134, 244),
-      metallic: 1.0,
-    );
-    final cube = ArCoreCube(
-      materials: [material],
-      size: vector.Vector3(0.5, 0.5, 0.5),
-    );
-    final node = ArCoreNode(
-      shape: cube,
-      position: vector.Vector3(-0.5, 0.5, -3.5),
-    );
-    controller.addArCoreNode(node);
-  }
-
-  @override
-  void dispose() {
-    arCoreController.dispose();
-    super.dispose();
   }
 }
