a9756d727455880ec1954bc2c4458c0869e989be
Hrishikesh Vedantam <shikiikihs2@gmail.com>
2020-12-07 18:20:58 +0530

    bug fixes

--- lib/strings.dart
+++ lib/strings.dart
@@ -3,8 +3,8 @@ String statuePrefab = "statueModel";
 String cubePrefab = "cubeModel";
 String catPrefab = "catModel";
 String legoHousePrefab = "legoHouseModel";
-String aboutApp = "LearnARF is a Flutter application which uses "
+String aboutApp = "ARZone is a Flutter application which uses "
     "Google's ARCore to spawn 3D models into the real world. "
     "Additionally, it uses the Sceneform SDK to make it easier "
     "to handle 3D models.";
-String repoLink = "https://github.com/akshansh2000/learn_ar_flutter";
+String repoLink = "https://github.com/Shiki992/arzone";
